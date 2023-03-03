d8574e902eb3755db449e654575357c2a09d9d70
Nabawy Hussein <37081451+nabawyhussein@users.noreply.github.com>
2023-03-03 19:02:28 +0200

    basicImagesPath

--- lib/utils/resources/assets_manger.dart
+++ lib/utils/resources/assets_manger.dart
@@ -1,7 +1,6 @@
-const String IMAGE_PATH = "assets/images";
-const String ICON_PATH = "assets/icons";
-const String JSON_PATH = "assets/json";
+
 
 class AppImages {
-  static const appLogo = "$IMAGE_PATH/logo.png";
+  static const String basicImagesPath = "assets/images/";
+  static const appLogo = "${basicImagesPath}logo.png";
 }
